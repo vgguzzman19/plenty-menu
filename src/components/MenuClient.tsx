6f398a39aefb322e3dd31e8af70c92c6c900398d
vgguzzman19 <vgguzzman@gmail.com>
2026-06-27 15:02:03 +0100

    Restaura botón de reseña Google

--- src/components/MenuClient.tsx
+++ src/components/MenuClient.tsx
@@ -35,6 +35,7 @@ function rowToProduct(row: any): Product {
 }
 
 const INSTAGRAM_URL = "https://www.instagram.com/plenty.brunch/";
+const GOOGLE_REVIEW_URL = "https://www.google.com/maps/place/Plenty./@41.8141564,3.0614864,17z/data=!4m8!3m7!1s0x12bb016c5e50eecf:0x52e50d290df5464a!8m2!3d41.8141524!4d3.0640613!9m1!1b1!16s%2Fg%2F11z9rfspc0?entry=ttu&g_ep=EgoyMDI2MDYyNC4wIKXMDSoASAFQAw%3D%3D";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function rowToCategory(row: any): Category {
@@ -215,6 +216,17 @@ export function MenuClient({ categories: initialCategories, products: initialPro
           >
             <svg className="w-4 h-4 text-brand-honey/70" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+            </svg>
+          </a>
+          <a
+            href={GOOGLE_REVIEW_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Déjanos una reseña en Google"
+            className="flex items-center justify-center w-8 h-8 rounded-full bg-white/10 hover:bg-white/20 transition-colors"
+          >
+            <svg className="w-4 h-4 text-brand-honey/70" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
             </svg>
           </a>
         </div>
